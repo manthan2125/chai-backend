d3cdd57e143891d9a16268ce397a9489bd2ca352
Manthan <manthanrathore2172005@gmail.com>
2026-07-30 01:51:08 +0530

    Add user account management features: change password, get current user, update details, and avatar

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -257,10 +257,117 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 
 })
 
+const changeCurrentPassword = asyncHandler(async(req, res) => {
+    const { oldPassword, newPassword } = req.body;
+
+    const user = await userModel.findById(req.user?._id);
+    const isPasswordValid = await user.isPasswordCorrect(oldPassword)
+
+    if(!isPasswordValid){
+        throw new ApiError(400, "Invalid Old Password")
+    }
+
+    user.password = newPassword;
+    await user.save({validateBeforeSave : false})
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,{}, "Passsword changes successfully"))
+})
+
+const getCurrentUser = asyncHandler(async(req, res) => {
+    return res
+    .status(200)
+    .json(new ApiResponse(200, req.user, "Current user fetched successfully"))
+})
+
+const updateAccountDetails = asyncHandler(async(req, res) => {
+    const {fullName, email} = req.body;
+
+    if(!fullName || !email){
+        throw new ApiError(400, "All fileds are required")
+    }
+
+    const user = await userModel.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set : {
+                fullName : fullName,
+                email
+            }
+        },
+        {new: true}  // update hone ke baad updatedUser return hoga user var ke andar
+    ).select("-password -refreshToken")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully"))
+
+
+})
+
+const updateUserAvatar = asyncHandler(async(req, res) => {
+    const avatarLocalPath = req.file?.path;
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar file is missing")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)  // this avatar is an object returned by uploadOnCloudinary 
+    if(!avatar.url){
+        throw new ApiError(400, "Error while uploading on avatar")
+    }
+
+    const user = await userModel.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set : {
+                avatar: avatar.url
+            }
+        },
+        {new: true}
+    ).select("-password -refreshToken")
+
+     return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Avatar updated successfully"))
+
+})
+
+const updateUserCoverImage = asyncHandler(async(req, res) => {
+    const coverImageLocalPath = req.file?.path;
+    if(!coverImageLocalPath){
+        throw new ApiError(400, "CoverImage file is missing")
+    }
+
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)  // this avatar is an object returned by uploadOnCloudinary 
+    if(!coverImage.url){
+        throw new ApiError(400, "Error while uploading on coverImage")
+    }
+
+    const user = await userModel.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set : {
+                coverImage: coverImage.url
+            }
+        },
+        {new: true}
+    ).select("-password -refreshToken")
+    
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Cover Image updated successfully"))
+})
+
 
 export { 
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage
  }
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,26 +5,26 @@ import { userModel } from "../models/user.model.js";
 
 
 // Authorization: Bearer <token>
-export const verifyJWT = asyncHandler( async (req, _, next) => {
+export const verifyJWT = asyncHandler(async (req, _, next) => {
     // kabhi esi situation aegi jhn par res ka usage nhi ho rha hoga whn aap res ki jagah _ laga skte hain - production mein esi chizein milengi
-    try{
-        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
 
-    if(!token) {
-        throw new ApiError(401, "Unauthorised request")
-    }
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        if (!token) {
+            throw new ApiError(401, "Unauthorised request")
+        }
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
 
-    const user = await userModel.findById(decodedToken?._id).select("-password -refreshToken")
-    if(!user) {
-        // next_vdo : discussion about frontend
-        throw new ApiError(401, "Invalid access token")
-    }
+        const user = await userModel.findById(decodedToken?._id).select("-password -refreshToken")
+        if (!user) {
+            
+            throw new ApiError(401, "Invalid access token")
+        }
 
-    req.user = user;
-    next();
+        req.user = user;
+        next();
     }
-    catch(err){
+    catch (err) {
         throw new ApiError(401, err?.message || "Invalid access token")
-    } 
+    }
 })
