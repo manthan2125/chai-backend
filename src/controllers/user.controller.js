1c1cb6021ac6d1b0e205907b5ed33e07b2bbe5d2
Manthan <manthanrathore2172005@gmail.com>
2026-07-30 19:47:35 +0530

    Add getWatchHistory endpoint: implement aggregation to fetch user's watch history with video owner details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -447,7 +447,56 @@ const getUserChannelProfile = asyncHandler(async(req, res) => {
     .json(
         new ApiResponse(200, channel[0], "User channel fetched successfully")
     )
-});
+})
+
+// see lec 20 and 21 again 
+const getWatchHistory = asyncHandler(async (req, res) => {
+    const user = await userModel.aggregate([
+        {
+            $match: new mongoose.Types.ObjectId(req.user._id)
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user[0].watchHistory, "Watch History fetched successfully")
+    )
+})
 
 
 export {
