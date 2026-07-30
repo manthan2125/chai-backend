d64523c49541500445ad9d21d661ad9fd931d276
Manthan <manthanrathore2172005@gmail.com>
2026-07-30 20:45:48 +0530

    Add models for comments, likes, playlists, and tweets; update user routes for cover image handling

---
+++ src/models/like.model.js
@@ -0,0 +1,22 @@
+import mongoose,{ Schema } from "mongoose";
+
+const likeSchema = new Schema({
+    video: {
+        type: Schema.Types.ObjectId,
+        ref: "video"
+    },
+    comment: {
+        type: Schema.Types.ObjectId,
+        ref: "comment"
+    },
+    tweet: {
+        type: Schema.Types.ObjectId,
+        ref: "tweet"
+    },
+    likedBy: {
+        type: Schema.Types.ObjectId,
+        ref: "user"
+    },
+}, {timestamps: true})
+
+export const likeModel = mongoose.model("like", likeSchema)
---
+++ src/models/tweet.model.js
@@ -0,0 +1,16 @@
+import mongoose, { Schema } from "mongoose";
+
+const tweetSchema = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "user"
+    },
+
+    
+}, {timestamps:true})
+
+export const tweetModel = mongoose.Schema("tweet", tweetSchema)
